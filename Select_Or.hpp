66e1421f104ad287709be115eaad64984c7a3ce8
toothnnail <michael.s.dong@gmail.com>
2021-05-04 03:56:31 -0700

    Select.Or was updated

---
+++ Select_Or.hpp
@@ -0,0 +1,13 @@
+#ifndef __SELECT_OR_HPP__
+#define __SELECT_OR_HPP__
+
+#include <cstring>
+#include "select.hpp"
+
+class Select_Or: public Select {
+public:
+	Select_Or(Select* selectionOne, Select* selectionTwo) : One(selectionOne), Two(selectionTwo) {}
+	bool select(const Spreadsheet* sheet, int row) const{ return One.select(sheet, row) || Two.select(sheet, row); }
+};
+
+#endif //__SELECT_OR_HPP__
